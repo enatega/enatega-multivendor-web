f057234d02f5d91b267571e7b1e2e85857d78dce
Faizan1421 <faizantayyab37@gmail.com>
2025-03-27 16:53:11 +0500

    updated

--- lib/utils/assets/svg/index.tsx
+++ lib/utils/assets/svg/index.tsx
@@ -6,8 +6,5 @@ export { default as RatingSvg } from "./rating";
 export { default as InfoSvg } from "./info";
 export { default as ClockSvg } from "./clock";
 export { default as CircleCrossSvg } from "./circle-cross";
-<<<<<<< HEAD
 export { default as MenuSvg } from "./menu";
-=======
 export { default as CartSvg } from "./cart";
->>>>>>> d2bc0c9d1fc7a7c9da2554b12213421bfb019de4
