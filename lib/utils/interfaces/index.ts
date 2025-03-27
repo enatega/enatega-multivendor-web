a5034006f5920ac964bad67ce284978d5eced5cc
Umer Farooq <umer_farooq@ninjascode.com>
2025-03-27 16:43:30 +0500

    develop - changeset - added cart ui - v4

--- lib/utils/interfaces/index.ts
+++ lib/utils/interfaces/index.ts
@@ -22,4 +22,5 @@ export * from "./svg.interface";
 export * from "./banner.interface";
 export * from "./slider-card.interface";
 export * from "./food-item-detail.interface";
+export * from "./cart.interface";
 export * from './icon-with-title.interface';
