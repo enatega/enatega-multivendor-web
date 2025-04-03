ab9d17b4cda49435769b1609547767020b3c271f
Faizan1421 <faizantayyab37@gmail.com>
2025-04-03 17:31:46 +0500

    Refactor comment input to use CustomTextAreaField component

--- lib/ui/screen-components/protected/profile/order-history/past-orders/rating/step-three/index.tsx
+++ lib/ui/screen-components/protected/profile/order-history/past-orders/rating/step-three/index.tsx
@@ -3,6 +3,7 @@ import ActionButton from "@/lib/ui/useable-components/action-button"
 import RenderAspects from "@/lib/ui/useable-components/render-aspects"
 import { IRenderStepThreeProps } from "@/lib/utils/interfaces/ratings.interface";
 import CustomButton from "@/lib/ui/useable-components/button";
+import CustomTextAreaField from "@/lib/ui/useable-components/custom-text-area-field";
 
 
   // Render the third step - Comment input with aspects selection
@@ -12,16 +13,16 @@ import CustomButton from "@/lib/ui/useable-components/button";
       <CustomButton
       label="+ Add a comment"
       className="!font-thin w-full py-2 rounded-full bg-gray-100 text-gray-400 mb-4 text-center"/>
-      <div className="w-full mb-4 border border-gray-200 rounded-lg">
-        <textarea
+      <div className="w-full mb-4  rounded-lg">
+        <CustomTextAreaField
           value={comment}
           onChange={(e) => setComment(e.target.value)}
           placeholder="Type here"
           className="w-full p-4 rounded-lg focus:outline-none resize-none"
           maxLength={200}
-          rows={4}
+          rows={5}
         />
-        <div className="flex justify-end p-2 text-gray-400 text-sm">{comment.length}/200</div>
+        <div className="flex justify-end py-2 text-gray-400 text-sm">{comment.length}/200</div>
       </div>
       <ActionButton onClick={handleSubmitDebounced} primary>
         Submit
