19ee8054b900c9ac0415927127df6038a9481554
Faizan1421 <faizantayyab37@gmail.com>
2025-04-07 21:38:28 +0500

    comment out ProfileLayoutScreen in ProfileRootLayout for debugging

--- app/(localized)/profile/layout.tsx
+++ app/(localized)/profile/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import ProfileLayoutScreen from "@/lib/ui/layouts/protected/profile";
+// import ProfileLayoutScreen from "@/lib/ui/layouts/protected/profile";
 
 export default function ProfileRootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ProfileLayoutScreen>{children}</ProfileLayoutScreen>;
+  // return <ProfileLayoutScreen>{children}</ProfileLayoutScreen>;
+  return <div>{children}</div>
 }
