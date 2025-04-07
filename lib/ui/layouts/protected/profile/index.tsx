713cef02e7c84077424a61416f918ee52f967b7f
Faizan1421 <faizantayyab37@gmail.com>
2025-04-07 21:14:09 +0500

    removed tabs temporarily for debugging

--- lib/ui/layouts/protected/profile/index.tsx
+++ lib/ui/layouts/protected/profile/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ProfileHeader from "@/lib/ui/screen-components/protected/layout/profile/profile-header";
-import ProfileTabs from "@/lib/ui/screen-components/protected/layout/profile/profile-tabs";
+// import ProfileTabs from "@/lib/ui/screen-components/protected/layout/profile/profile-tabs";
 import { PaddingContainer } from "@/lib/ui/useable-components/containers";
 import { IProtectedProfileLayoutComponent } from "@/lib/utils/interfaces";
 import { useRouter } from "next/navigation";
@@ -32,7 +32,7 @@ export default function ProfileLayout({
       <div className="w-screen h-screen flex flex-col pb-20">
         <div className="flex flex-col justify-center space-y-6 items-center py-4 px-4  md:px-6 lg:px-12 xl:px-20 2xl:px-[80px] ">
           <ProfileHeader/>
-          <ProfileTabs />
+          {/* <ProfileTabs /> */}
         </div>
         <div className="flex-1 overflow-auto px-4 md:px-0 lg:px-28 xl:px-40">  
           {/* Scrollable Content */}
