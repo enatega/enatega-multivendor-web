8255be0e58eab782b384707a0905338ea1e3693f
Faizan1421 <faizantayyab37@gmail.com>
2025-04-07 18:00:20 +0500

    update the next js config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
