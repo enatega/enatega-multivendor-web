7d03f8f5af0f5d48407547ca9845849a9f096701
Faizan1421 <faizantayyab37@gmail.com>
2025-04-07 21:50:53 +0500

    comment out components in PersonalInfoScreen for debugging

--- lib/ui/screens/protected/profile/personal-info/index.tsx
+++ lib/ui/screens/protected/profile/personal-info/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { FavouriteProducts, PersonalInfoMain } from "@/lib/ui/screen-components/protected/profile";
+// import { FavouriteProducts, PersonalInfoMain } from "@/lib/ui/screen-components/protected/profile";
 
   export default function PersonalInfoScreen() {
     return (
       <div className="flex flex-col space-y-10 my-10">
         {/* Main Profile */}
-       <PersonalInfoMain/>
+       {/* <PersonalInfoMain/> */}
        {/* Favourites Items  */}
-       <FavouriteProducts/>
+       {/* <FavouriteProducts/> */}
       </div>
     );
   }
