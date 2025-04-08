bfe6c66487faf54bdd379d267ac120eaadf41d79
Umer Farooq <umer_farooq@ninjascode.com>
2025-04-08 10:10:48 +0500

    umer - changeset - added skeelton to the restaurant - v3

--- lib/ui/useable-components/custom-skeletons/inputfield.skeleton.tsx
+++ lib/ui/useable-components/custom-skeletons/inputfield.skeleton.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "primereact/skeleton";
 
-const InputSkeleton = ({ showLabel = true }: { showLabel: boolean }) => {
+const InputSkeleton = ({ showLabel = true }: { showLabel?: boolean }) => {
   return (
     <div className="w-full space-y-2">
       {showLabel && <Skeleton width="15%" height="0.8rem" className="h-10" />}
