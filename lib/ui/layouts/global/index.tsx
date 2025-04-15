d18eced24419e5871fffc70fbb6802216ea8c2f6
Umer Farooq <umer_farooq@ninjascode.com>
2025-04-15 10:21:51 +0500

    umer - changeset - added google provider to the root - v1

--- lib/ui/layouts/global/index.tsx
+++ lib/ui/layouts/global/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // Components
 import AppTopbar from "@/lib/ui/screen-components/un-protected/layout/app-bar";
 
@@ -11,16 +11,22 @@ import { IProvider } from "@/lib/utils/interfaces";
 // Google OAuth
 import AuthModal from "@/lib/ui/screen-components/un-protected/authentication";
 import AppFooter from "../../screen-components/un-protected/layout/app-footer";
+import { useConfig } from "@/lib/context/configuration/configuration.context";
+import { GoogleMapsProvider } from "@/lib/context/global/google-maps.context";
 
 const AppLayout = ({ children }: IProvider) => {
   // States
   const [isAuthModalVisible, setIsAuthModalVisible] = useState(false);
 
+  // Hook
+  const { GOOGLE_MAPS_KEY, LIBRARIES } = useConfig();
+
   // Handlers
   const handleModalToggle = () => {
     setIsAuthModalVisible((prev) => !prev);
   };
-  return (
+
+  const UI = (
     <div className="layout-main">
       <div className="layout-top-container">
         <AppTopbar handleModalToggle={handleModalToggle} />
@@ -29,7 +35,7 @@ const AppLayout = ({ children }: IProvider) => {
         <div className="layout-main">{children}</div>
       </div>
       <div>
-        <AppFooter/>
+        <AppFooter />
       </div>
       <AuthModal
         handleModalToggle={handleModalToggle}
@@ -37,6 +43,14 @@ const AppLayout = ({ children }: IProvider) => {
       />
     </div>
   );
+
+  useEffect(() => {}, [GOOGLE_MAPS_KEY]);
+
+  return GOOGLE_MAPS_KEY ?
+      <GoogleMapsProvider apiKey={GOOGLE_MAPS_KEY} libraries={LIBRARIES}>
+        <>{UI}</>
+      </GoogleMapsProvider>
+    : <>{UI}</>;
 };
 
 export default AppLayout;
--- lib/ui/useable-components/address/index.tsx
+++ lib/ui/useable-components/address/index.tsx
@@ -19,6 +19,7 @@ import useUser from "@/lib/hooks/useUser";
 
 // Interface
 import {
+  IDropdownSelectItem,
   IUserAddress,
   IUserAddressComponentProps,
 } from "@/lib/utils/interfaces";
@@ -26,6 +27,10 @@ import {
 // API
 import { SELECT_ADDRESS } from "@/lib/api/graphql";
 import { AnimatePresence, motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import CustomTextField from "../input-field";
+import CustomDropdownComponent from "../custom-dropdown";
 
 /////
 
@@ -83,56 +88,101 @@ export default function UserAddressComponent(
 
   // Sub-Components
   const CHOOSE_ADDRESS = (
-    <div className="w-full bg-red-500">
-      {loadingProfile ?
-        <div className="w-full flex items-center justify-center m-4">
-          <CustomLoader />
-        </div>
-      : profile?.addresses.map((address, index) => (
-          <div
-            key={index}
-            className="w-full mb-4 flex items-center justify-between"
-          >
-            <div className="w-full flex items-center gap-x-2">
-              <div className="p-2 bg-gray-50 rounded-full">
-                <OfficeSvg color={address.selected ? "#0EA5E9" : undefined} />
-              </div>
-              <div className="w-full flex flex-col gap-y-[2px]">
-                <span
-                  className={`font-inter font-medium text-sm leading-5 tracking-normal ${address.selected ? "text-sky-500" : "text-gray-500"}`}
-                >
-                  {address.label}
-                </span>
-                <span
-                  className={`font-inter font-normal text-xs leading-4 tracking-normal ${address.selected ? "text-sky-400" : "text-gray-400"}`}
-                >
-                  {address.deliveryAddress}
-                </span>
-              </div>
-            </div>
-            {!address.selected && (
-              <div>
-                <CustomButton
-                  label="Choose"
-                  rounded
-                  loading={modifiyingId === address._id && loading}
-                  className="border p-2 pl-4 pr-4 border-gray-300 text-sky-500 font-medium"
-                  onClick={() => onHandleSelectAddress(address)}
-                />
-              </div>
-            )}
+    <div className="w-full space-y-4">
+      {/* Header */}
+      <div className="w-full">
+        <span className="font-inter font-bold text-[25px] tracking-normal">
+          Where to?
+        </span>
+      </div>
+
+      <div className="w-full flex flex-col items-center">
+        {loadingProfile ?
+          <div className="w-full flex items-center justify-center m-4">
+            <CustomLoader />
           </div>
-        ))
-      }
-      <button
-        className="bg-[#5AC12F] text-gray-900 w-full py-2 rounded-full text-xs lg:text-[12px]"
-        onClick={() => paginate(1)}
-      >
-        Add new address
-      </button>
+        : profile?.addresses.map((address, index) => (
+            <div
+              key={index}
+              className="w-full mb-4 flex items-center justify-between"
+            >
+              <div className="w-full flex items-center gap-x-2">
+                <div className="p-2 bg-gray-50 rounded-full">
+                  <OfficeSvg color={address.selected ? "#0EA5E9" : undefined} />
+                </div>
+                <div className="w-full flex flex-col gap-y-[2px]">
+                  <span
+                    className={`font-inter font-medium text-sm leading-5 tracking-normal ${address.selected ? "text-sky-500" : "text-gray-500"}`}
+                  >
+                    {address.label}
+                  </span>
+                  <span
+                    className={`font-inter font-normal text-xs leading-4 tracking-normal ${address.selected ? "text-sky-400" : "text-gray-400"}`}
+                  >
+                    {address.deliveryAddress}
+                  </span>
+                </div>
+              </div>
+              {!address.selected && (
+                <div>
+                  <CustomButton
+                    label="Choose"
+                    rounded
+                    loading={modifiyingId === address._id && loading}
+                    className="border p-2 pl-4 pr-4 border-gray-300 text-sky-500 font-medium"
+                    onClick={() => onHandleSelectAddress(address)}
+                  />
+                </div>
+              )}
+            </div>
+          ))
+        }
+        <button
+          className="w-[90%] h-fit bg-[#5AC12F] text-gray-900 py-2 rounded-full text-base lg:text-[14px]"
+          onClick={() => paginate(1)}
+        >
+          <FontAwesomeIcon icon={faPlus} /> <span>Add new address</span>
+        </button>
+      </div>
     </div>
   );
-  const ADD_ADDRESS = <div className="w-full">Add Address</div>;
+  const ADD_ADDRESS = (
+    <div className="w-full">
+      {/* Google Maps */}
+      <div className="w-full">Google Maps</div>
+
+      {/* Header */}
+      <div className="w-full">
+        <span className="font-inter font-semibold text-[18px] tracking-normal">
+          Add new address
+        </span>
+      </div>
+
+      <div className="w-full flex flex-col items-center gap-y-2">
+        <div className="w-full space-y-2">
+          <CustomDropdownComponent
+            name={""}
+            placeholder={""}
+            selectedItem={null}
+            setSelectedItem={function (
+              key: string,
+              item: IDropdownSelectItem
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+            options={[]}
+          />
+          <CustomTextField type="text" name="Address" showLabel={false} />
+        </div>
+        <button
+          className="w-[90%] h-fit bg-[#5AC12F] text-gray-900 py-2 rounded-full text-base lg:text-[14px]"
+          onClick={() => paginate(1)}
+        >
+          <FontAwesomeIcon icon={faPlus} /> <span>Add new address</span>
+        </button>
+      </div>
+    </div>
+  );
   const SET_ADDRESS_TYPE = <div className="w-full"> Set Address Type </div>;
 
   const COMPONENTS_LIST = [CHOOSE_ADDRESS, ADD_ADDRESS, SET_ADDRESS_TYPE];
@@ -151,23 +201,12 @@ export default function UserAddressComponent(
           initial="enter"
           animate="center"
           exit="exit"
-          transition={{ duration: 0.4 }}
-          className="absolute w-full bg-green-400"
+          transition={{ duration: 0.2 }}
+          className="w-full relative" // changed from absolute to relative
         >
-          <div className="w-full">
-            {/* Header */}
-            <div className="w-full">
-              <span className="font-inter font-bold text-[25px]  tracking-normal">
-                Where to?
-              </span>
-            </div>
-
-            {COMPONENTS_LIST[index]}
-          </div>
+          {COMPONENTS_LIST[index]}
         </motion.div>
       </AnimatePresence>
-
-      {/* Main */}
     </Dialog>
   );
 }
