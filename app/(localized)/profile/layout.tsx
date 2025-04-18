6a43665da0f052128771b445b0e8e46109e51c6f
Umer Farooq <umer_farooq@ninjascode.com>
2025-04-18 09:49:09 +0500

    umer - changeset - added auth guard - v3

--- app/(localized)/profile/layout.tsx
+++ app/(localized)/profile/layout.tsx
@@ -1,44 +1,15 @@
 "use client";
 
-import { useAuth } from "@/lib/context/auth/auth.context";
 import AuthGuard from "@/lib/hoc/auth.guard";
+
+// Layout
 import ProfileLayoutScreen from "@/lib/ui/layouts/protected/profile";
-import { usePathname, useRouter } from "next/navigation";
-import { useLayoutEffect } from "react";
 
 export default function ProfileRootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { authToken } = useAuth();
-  const pathname = usePathname();
-  const router = useRouter();
-
-  // Handlers
-  const onHandleUserAuthenticate = () => {
-    try {
-      if (!authToken) {
-        const previousUrl = document.referrer;
-        const isSameOrigin = previousUrl.startsWith(window.location.origin);
-        const previousPath =
-          isSameOrigin ? new URL(previousUrl).pathname : null;
-
-        if (previousPath && previousPath !== pathname) {
-          router.back();
-        } else {
-          router.push("/");
-        }
-      }
-    } catch (err) {
-      router.replace("/");
-    }
-  };
-
-  useLayoutEffect(() => {
-    onHandleUserAuthenticate();
-  }, [authToken]);
-
   const ProtectedLayout = AuthGuard(
     ({ children }: { children: React.ReactNode }) => {
       return <ProfileLayoutScreen>{children}</ProfileLayoutScreen>;
