5b03cb9bf9e9597432b423993cfa7a9e123af00a
Umer Farooq <umer_farooq@ninjascode.com>
2025-04-22 10:05:07 +0500

    umer - fix - added not-found lottie file - v3

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,12 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 // ANimation
-import notFoundAnimation from "@/lib/assets/animations/404.json"; // You can place your JSON file in public or src/assets
+// import notFoundAnimation from "@/lib/assets/animations/404.json"; // You can place your JSON file in public or src/assets
+import animationData from "@/lib/assets/animations/404.json"; // Your Lottie file
 
 export default function NotFound() {
   const router = useRouter();
@@ -22,7 +24,7 @@ export default function NotFound() {
     <div className="flex items-center justify-center h-screen bg-trasnsparent px-4">
       <div className="text-center max-w-md w-full bg-transparent">
         <Lottie
-          animationData={notFoundAnimation}
+          animationData={animationData}
           loop
           className="h-64 mx-auto bg-black"
         />
