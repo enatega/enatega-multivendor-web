10217b57cec6ac920ec57b2a80b273433978371c
Faizan1421 <faizantayyab37@gmail.com>
2025-04-28 12:10:19 +0500

    Add "use client" directive to TrackingOrderDetails component

--- lib/ui/screen-components/protected/order-tracking/components/tracking-order-details.tsx
+++ lib/ui/screen-components/protected/order-tracking/components/tracking-order-details.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import { IOrderTrackingDetail } from "@/lib/utils/interfaces/order-tracking-detail.interface";
