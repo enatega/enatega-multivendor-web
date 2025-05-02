215f7f083e616632064336b06a7fd5fe1efc767c
Hasnat Amin <hasnatamin065@gmail.com>
2025-05-02 12:43:24 +0500

    updated name modal to show all name

--- lib/ui/screen-components/protected/profile/settings/main/update-name/index.tsx
+++ lib/ui/screen-components/protected/profile/settings/main/update-name/index.tsx
@@ -29,35 +29,37 @@ export default function NameUpdateModal({
   existedName: string;
 }) {
 
-    // States
-    const [firstName, lastName] = existedName?.split(" ") || ["", ""];
-    const [formData, setFormData] = useState<UserFormData>({
-        firstName: "",
-        lastName: "",
-    });
+  // States
+  const nameParts = existedName?.trim().split(" ") || [];
+  const [firstName, ...rest] = nameParts;
+  const lastName = rest.join(" ");
+  const [formData, setFormData] = useState<UserFormData>({
+    firstName: "",
+    lastName: "",
+  });
 
-// Hooks    
-    const { showToast } = useToast();
+  // Hooks    
+  const { showToast } = useToast();
 
-    // Queries and Mutations
-    
-   // refetch user profile after updating name
-     const [
-        fetchProfile
-      ] = useLazyQuery(GET_USER_PROFILE, {
-        fetchPolicy: "network-only",
-      });
-     
-   // Update user muattion 
+  // Queries and Mutations
+
+  // refetch user profile after updating name
+  const [
+    fetchProfile
+  ] = useLazyQuery(GET_USER_PROFILE, {
+    fetchPolicy: "network-only",
+  });
+
+  // Update user muattion 
   const [updateUser] = useMutation(UPDATE_USER, {
     onCompleted: () => {
-        fetchProfile()
-        showToast({
-            type: "success",
-            title: "Success",
-            message: "User profile updated successfully",
-        });
-        handleUpdateNameModal?.();
+      fetchProfile()
+      showToast({
+        type: "success",
+        title: "Success",
+        message: "User profile updated successfully",
+      });
+      handleUpdateNameModal?.();
     },
     onError: (error) => {
       showToast({
@@ -67,14 +69,14 @@ export default function NameUpdateModal({
       });
     },
   });
-   
+
   // handlers
   // Handle input change
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
-  
+
   // handle submit
   const handleSubmit = useDebounceFunction(() => {
     const fullName = `${formData.firstName} ${formData.lastName}`.trim();
@@ -84,7 +86,7 @@ export default function NameUpdateModal({
       },
     });
   },
-  500 // Debounce time in milliseconds
+    500 // Debounce time in milliseconds
   );
 
   // Handle cancel
@@ -98,63 +100,63 @@ export default function NameUpdateModal({
   };
 
   // useeffects
-   
-  useEffect(() => { 
+
+  useEffect(() => {
     setFormData({
       firstName: firstName || "",
       lastName: lastName || "",
     });
   }, [existedName]);
-   
+
   return (
     <CustomDialog
       visible={isUpdateNameModalVisible}
       onHide={handleUpdateNameModal}
       width="600px"
-    > 
-        <div className="flex flex-col  items-center  p-4">
-          <div className=" flex items-center justify-center">
-            <LaptopSvg width={250} height={250} />
-          </div>
-          <div className="w-full">
-            <div className="">
-              <h2 className="text-2xl font-extrabold mb-10 text-black">Name</h2>
-              <div className="p-float-label mb-6">
-                <InputText
-                  id="firstName"
-                  name="firstName"
-                  value={formData.firstName}
-                  onChange={handleInputChange}
-                  className="w-full p-3 border-2 border-gray-300 rounded-md"
-                />
-                <label htmlFor="firstName">First name</label>
-              </div>
-              <div className="p-float-label">
-                <InputText
-                  id="lastName"
-                  name="lastName"
-                  value={formData.lastName}
-                  onChange={handleInputChange}
-                  className="w-full p-3 border-2 border-gray-300 rounded-md"
-                />
-                <label htmlFor="lastName">Last name</label>
-              </div>
+    >
+      <div className="flex flex-col  items-center  p-4">
+        <div className=" flex items-center justify-center">
+          <LaptopSvg width={250} height={250} />
+        </div>
+        <div className="w-full">
+          <div className="">
+            <h2 className="text-2xl font-extrabold mb-10 text-black">Name</h2>
+            <div className="p-float-label mb-6">
+              <InputText
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                className="w-full p-3 border-2 border-gray-300 rounded-md"
+              />
+              <label htmlFor="firstName">First name</label>
+            </div>
+            <div className="p-float-label">
+              <InputText
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                className="w-full p-3 border-2 border-gray-300 rounded-md"
+              />
+              <label htmlFor="lastName">Last name</label>
             </div>
           </div>
         </div>
-        <div className="flex flex-row w-full justify-between  gap-2 md:gap-0 px-4 ">
-          <CustomButton
-            label={"Cancel"}
-             className="bg-white flex items-center justify-center rounded-full border border-gray-300 p-3 w-full md:w-[268px] h-14 text-lg font-medium"
-            onClick={handleCancel}
-          />
+      </div>
+      <div className="flex flex-row w-full justify-between  gap-2 md:gap-0 px-4 ">
+        <CustomButton
+          label={"Cancel"}
+          className="bg-white flex items-center justify-center rounded-full border border-gray-300 p-3 w-full md:w-[268px] h-14 text-lg font-medium"
+          onClick={handleCancel}
+        />
 
-          <CustomButton
-            label={"Save"}
-           className="bg-[#5AC12F] text-white flex items-center justify-center rounded-full p-3 w-full md:w-[268px] mb-4 h-14 text-lg font-medium"
-            onClick={handleSubmit}
-          />
-        </div>
+        <CustomButton
+          label={"Save"}
+          className="bg-[#5AC12F] text-white flex items-center justify-center rounded-full p-3 w-full md:w-[268px] mb-4 h-14 text-lg font-medium"
+          onClick={handleSubmit}
+        />
+      </div>
     </CustomDialog>
   );
 }
