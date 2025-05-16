a6de61c8cc16a9e479706b88c2eb2f24674221bb
Zeeshan Rafiq <102378715+ZeeshanRafiq38@users.noreply.github.com>
2025-05-16 15:25:56 +0500

    add no-data animation for empty restaurant list

--- app/(localized)/mapview/[slug]/page.tsx
+++ app/(localized)/mapview/[slug]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import useNearByRestaurantsPreview from "@/lib/hooks/useNearByRestaurantsPreview";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import DisplayError from "./components/DisplayError";
 import { useConfig } from "@/lib/context/configuration/configuration.context";
 import Map from "./components/Map";
 import SideList from "./components/SideList";
+import Lottie from "lottie-react";
 
 interface MapViewPageProps {
     params: {
@@ -14,6 +15,15 @@ interface MapViewPageProps {
 }
 
 const MapView: FC<MapViewPageProps> = ({ params }) => {
+    const [animationData, setAnimationData] = useState<null | object>(null);
+
+    useEffect(() => {
+        fetch("/assets/lottie/no-results.json")
+            .then((res) => res.json())
+            .then(setAnimationData)
+            .catch((err) => console.error("Failed to load Lottie JSON", err));
+    }, []);
+
     const { slug } = params;
     const { error, loading, restaurantsData, groceriesData } =
         useNearByRestaurantsPreview();
@@ -49,10 +59,22 @@ const MapView: FC<MapViewPageProps> = ({ params }) => {
                 </div>
             ) : (
                 <div className="flex mt-1 relative min-h-screen max-h-screen md:flex-row flex-col-reverse">
-                    <div className="md:relative absolute bottom-0 z-[99] md:flex-[0.35] xl:flex-[0.25] overflow-y-auto md:w-auto w-full">
-                        <h1 className="text-2xl font-bold text-gray-900 h-screen flex items-center justify-center">
+                    <div className="md:relative absolute top-2 mt-12 z-[99] md:flex-[0.35] xl:flex-[0.25] overflow-y-auto md:w-auto w-full ">
+                        {/* <h1 className="text-2xl font-bold text-gray-900 h-screen flex items-center justify-center">
                             No data available to show.
-                        </h1>
+                        </h1> */}
+                        <div className="flex flex-col items-center justify-center p-6">
+                            <div className="w-32 h-32 md:w-60 md:h-60 flex items-center justify-center">
+                                <Lottie
+                                    animationData={animationData}
+                                    loop={true}
+                                    autoplay={true}
+                                />
+                            </div>
+                            <p className="text-gray-500 text-sm md:text-base text-center mb-4">
+                                No data available to show.
+                            </p>
+                        </div>
                     </div>
                     <div className="flex-[0.65] xl:flex-[0.75] h-screen overflow-y-auto hidden md:flex">
                         <Map
