27534c1326b4262a23c8f1478bd8948250d317ac
Syed Talha Ejaz <tsyed225@gmail.com>
2025-05-16 17:48:26 +0500

    handled the build error in the enatega web

--- app/(localized)/(singleVendor)/seeAllFavorite/page.tsx
+++ app/(localized)/(singleVendor)/seeAllFavorite/page.tsx
@@ -1,5 +1,7 @@
 // lib/ui/screens/protected/singleVendor/seeAllFavorite/index.tsx
 "use client";
+import React from "react";
+
 import SeeAllFavoriteFoodsScreen from "@/lib/ui/screens/protected/singleVendor/seeAllFavorite";
 export default function SeeAllFavoriteFoodsPage() {
 
