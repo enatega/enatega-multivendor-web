dd7f9f6967cab19d363db7d797adae2979431412
Umer F <uf.umerfarooq67@gmail.com>
2025-05-27 15:34:40 +0500

    updated backend url with develop

--- environment.ts
+++ environment.ts
@@ -1,8 +1,8 @@
 export default function getEnv(env: "DEV" | "PROD") {
   if (env === "PROD" || typeof window === "undefined") {
     return {
-     SERVER_URL: "http://v1-api-enatega-multivendor-develop.up.railway.app/",
-      WS_SERVER_URL: "ws://v1-api-enatega-multivendor-develop.up.railway.app/",
+     SERVER_URL: "https://v1-api-enatega-multivendor-develop.up.railway.app/",
+      WS_SERVER_URL: "wss://v1-api-enatega-multivendor-develop.up.railway.app/",
     };
   } else {
     return {
