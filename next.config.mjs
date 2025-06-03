268f05121d20828dc7eb28aa8a73e5d8a24d6a54
Ahmed Siddique <ch379796@gmail.com>
2025-06-03 20:46:35 +0500

    trying to fix issue of web server is not https

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,36 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests'
+          },
+          // Optional: Add other security headers
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
+    ]
+  },
   images: {
     // domains: ["storage.googleapis.com"],
     dangerouslyAllowSVG: true,
